da6b9fe4108c546d8f00ab1205f6f9ed878567c2
Ashwin Karollil <ashwin.karollil@sap.com>
2019-07-20 17:54:23 +0530

    Implement Registration Number by color functionality

--- src/main/java/com/design/entity/factories/RegnNumCar.java
+++ src/main/java/com/design/entity/factories/RegnNumCar.java
@@ -1,9 +1,12 @@
 package com.design.entity.factories;
 
+import com.design.entity.ParkingSlot;
 import com.design.utilities.ParkingLotConstants;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class RegnNumCar implements ProcessFactory {
 
@@ -14,12 +17,33 @@ public class RegnNumCar implements ProcessFactory {
     }
 
     /**
-     *
+     * Override executeCommand
      * @param command
      */
 
     @Override
     public void executeCommand(String command) {
 
+        String commandsString[] = command.split(" ");
+        String color = commandsString[1];
+        findRegistrationNumberByColor(color);
+    }
+
+
+    /**
+     * Find Registration Number By Color
+     * @param color
+     */
+
+    private void findRegistrationNumberByColor(String color) {
+
+        List<ParkingSlot> parkingSlotList = CreateParkingLot.getInstance().getParkingSlotList();
+        String registrationDetails =parkingSlotList.stream()
+                .filter(entry -> entry.getCar().getColor().equalsIgnoreCase(color))
+                .map(entry -> entry.getCar().getRegnNumber())
+                .collect(Collectors.joining(", "));
+
+        //SYS OUT
+        System.out.println(registrationDetails);
     }
 }
