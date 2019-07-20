a8d1a75205a284542592efa2b0f574f40b96704a
Ashwin Karollil <ashwin.karollil@sap.com>
2019-07-20 18:01:48 +0530

    Implement Slot Number by regn functionality

--- src/main/java/com/design/entity/factories/SlotNumRegn.java
+++ src/main/java/com/design/entity/factories/SlotNumRegn.java
@@ -1,9 +1,12 @@
 package com.design.entity.factories;
 
+import com.design.entity.ParkingSlot;
 import com.design.utilities.ParkingLotConstants;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class SlotNumRegn implements ProcessFactory {
 
@@ -14,12 +17,34 @@ public class SlotNumRegn implements ProcessFactory {
     }
 
     /**
-     *
+     * Override executeCommand
      * @param command
      */
 
     @Override
     public void executeCommand(String command) {
 
+        String commandsString[] = command.split(" ");
+        String regn = commandsString[1];
+        findSlotNumberByRegn(regn);
+    }
+
+
+    /**
+     * Find Slot Number By Regn
+     * @param regn
+     */
+    private void findSlotNumberByRegn(String regn) {
+        List<ParkingSlot> parkingSlotList = CreateParkingLot.getInstance().getParkingSlotList();
+        String slotDetails =parkingSlotList.stream()
+                .filter(entry -> entry.getCar().getRegnNumber().equalsIgnoreCase(regn))
+                .map(entry -> entry.getId().toString())
+                .collect(Collectors.joining(", "));
+
+        //SYS OUT
+        if(!slotDetails.isEmpty())
+            System.out.println(slotDetails);
+        else
+            System.out.println("Not found");
     }
 }
