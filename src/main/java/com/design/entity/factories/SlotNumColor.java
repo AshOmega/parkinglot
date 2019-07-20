c61bff09c236056f08cdc610ecfc0826292957b2
Ashwin Karollil <ashwin.karollil@sap.com>
2019-07-20 17:57:09 +0530

    Implement Slot Number by color functionality

--- src/main/java/com/design/entity/factories/SlotNumColor.java
+++ src/main/java/com/design/entity/factories/SlotNumColor.java
@@ -1,9 +1,12 @@
 package com.design.entity.factories;
 
+import com.design.entity.ParkingSlot;
 import com.design.utilities.ParkingLotConstants;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class SlotNumColor implements ProcessFactory {
 
@@ -14,12 +17,30 @@ public class SlotNumColor implements ProcessFactory {
     }
 
     /**
-     *
+     * Override executeCommand
      * @param command
      */
 
     @Override
     public void executeCommand(String command) {
+        String commandsString[] = command.split(" ");
+        String color = commandsString[1];
+        findSlotNumberByColor(color);
+    }
 
+
+    /**
+     * Find Slot Number By Color
+     * @param color
+     */
+    private void findSlotNumberByColor(String color) {
+        List<ParkingSlot> parkingSlotList = CreateParkingLot.getInstance().getParkingSlotList();
+        String slotDetails =parkingSlotList.stream()
+                .filter(entry -> entry.getCar().getColor().equalsIgnoreCase(color))
+                .map(entry -> entry.getId().toString())
+                .collect(Collectors.joining(", "));
+
+        //SYS OUT
+        System.out.println(slotDetails);
     }
 }
